a0f2df1a3d16332bfc114866d2b5543f3c720555
AminAnip <muhdnuramin002@gmail.com>
2025-06-09 22:12:04 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = defineConfig(
+  module.exports = {
+  publicPath: '/your-repo-name/',
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'Home - Amin'
+    }
+  }
+}
+)
