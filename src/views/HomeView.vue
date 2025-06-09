0ee494fdee7d934ef9b9708370f8ad5797b782b7
AminAnip <muhdnuramin002@gmail.com>
2025-06-09 22:53:20 +0800

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -27,7 +27,7 @@
     <div class="flex justify-center md:justify-start fadein-right"><img
   alt="avatar"
   class="w-60 h-60 md:w-72 md:h-72 rounded-full border-4 border-amber-200 pict"
-  src: 'https://i.ibb.co/gbRfLpJP/amin.jpg'/>
+  src= 'https://i.ibb.co/gbRfLpJP/amin.jpg'/>
     </div>
   </main>
 
